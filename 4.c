925cb3c4783b361a2671f19d39f115d372deed1b
vlazhv <danilabondarev1234@gmail.com>
2022-03-25 01:42:09 +0300

    atoi -> strtol

--- 4.c
+++ 4.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
 int main(int argc, char *argv[])
 {
@@ -8,6 +9,25 @@ int main(int argc, char *argv[])
 		perror("command file N\n file - file to read\n N - number of chars to read");
 		return -1;
 	}
+	char *endptr;
+	long int N = strtol(argv[2], &endptr, 10);
+	if (errno == ERANGE)
+	{
+		perror("N (number of chars to read) is out of range");
+		return -1;
+	}
+	if (endptr[0] == argv[2][0])
+	{
+		perror("invalid: N - number of chars to read");
+		return -1;
+	}
+	endptr = NULL;
+	if (N < 0)
+	{
+		perror("N < 0. If you wish to print all text input N = 0");
+		return -1;
+	}
+	
 	FILE *f;
 	if (!(f = fopen(argv[1], "r") ))
 	{
@@ -15,7 +35,6 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	int N = atoi(argv[2]);
 	int start = (N == 0 ? 1 : 0);
 	int c;
 	int flag = 1;
---
+++ README.md
@@ -0,0 +1,20 @@
+task 2:
+	print message
+
+task 3:
+	Terminal --> file
+
+task 4:
+	print N strings  file --> Terminal
+
+task 5:
+	copy file --> file
+
+task 6:
+	print content of current dir
+	print content of root dir
+
+
+task 7_var8:
+	for given dir and sub-dirs count SIZE and DISK USAGE of files
+	count (DISK USAGE of given dir) / (DISK USAGE '/')
